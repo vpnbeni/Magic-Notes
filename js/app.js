cd4b0a2279312b82374c05598af36bcfc2623dc2
Vipin <97426806+vpnbeni@users.noreply.github.com>
2022-01-29 12:36:55 +0530

    Added Title

--- js/app.js
+++ js/app.js
@@ -4,26 +4,24 @@ showNotes();
 console.log("Project 1");
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
-  // let addTitle = document.getElementById("title");
-  // let addNote= document.getElementById("adTxt");
-  // let addTxt = {
-  //   title:addTitle,
-  //   note:addNote
-  // }
-  let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
+  let addNote = document.getElementById("addNote");
+  let addTxt = {
+    title: addTitle.value,
+    note: addNote.value,
+  };
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  notesObj.push(addTxt);
   localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-  // console.log(notesObj);
+  addTitle.value = "";
+  addNote.value = "";
   showNotes();
 });
-
 //function to show elemtents from localStorage
 function showNotes() {
   let notes = localStorage.getItem("notes");
@@ -33,12 +31,13 @@ function showNotes() {
     notesObj = JSON.parse(notes);
   }
   let html = "";
+
   notesObj.forEach(function (element, index) {
     html += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem">
           <div class="card-body">
-          <h5 class="card-title">Note ${index + 1}</h5>
-            <p class="card-text">${element}</p>
+          <h5 class="card-title">${element.title}</h5>
+            <p class="card-text">${element.note}</p>
             <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-sm btn-primary">Delete Note</button> <button id="${index}" onclick="markImp(this.id)" class="btn btn-danger btn-sm mx-2" id="addImp" >Mark Important</button>
           </div>
         </div>`;
